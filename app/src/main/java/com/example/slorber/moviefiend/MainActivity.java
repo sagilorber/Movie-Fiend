915758138a2b8996af5422336f83e186a7cb2588
sagilorber <sagilorber8@gmail.com>
2016-09-08 11:44:09 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/slorber/moviefiend/MainActivity.java
+++ app/src/main/java/com/example/slorber/moviefiend/MainActivity.java
@@ -2,12 +2,75 @@ package com.example.slorber.moviefiend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    MovieContainer mc;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private CardAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        final String url = "http://api.themoviedb.org/3/movie/now_playing?api_key=f8546d2d948cd245e6cdb9d4332e6ca6";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Gson g = new Gson();
+                        mc = g.fromJson(response.toString(), MovieContainer.class);
+                        mLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
+                        mAdapter = new CardAdapter(mc.getMovies());
+                        mRecyclerView.setLayoutManager(mLayoutManager);
+                        mRecyclerView.setAdapter(mAdapter);
+
+                        mRecyclerView.addOnItemTouchListener(
+                                new RecyclerItemClickListener(MainActivity.this, new RecyclerItemClickListener.OnItemClickListener() {
+                                    @Override public void onItemClick(View view, int position) {
+
+
+
+                                    }
+                                })
+                        );
+
+                        //mc.setMovies(response.toString());
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        );
+
+// add it to the RequestQueue
+        queue.add(getRequest);
+
+
     }
+
+
 }
