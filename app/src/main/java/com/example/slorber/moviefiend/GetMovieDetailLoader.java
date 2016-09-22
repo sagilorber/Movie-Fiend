d9ee847216bf920e9352b636f8a1a762a76d2969
sagilorber <sagilorber8@gmail.com>
2016-09-22 14:09:56 +0300

    DeepLink

---
+++ app/src/main/java/com/example/slorber/moviefiend/GetMovieDetailLoader.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016 PayPal, Inc.
+ *
+ * All rights reserved.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ */
+
+package com.example.slorber.moviefiend;
+
+/**
+ * TODO: Write Javadoc for GetMovieDetailLoader.
+ *
+ * @author slorber
+ */
+public class GetMovieDetailLoader {
+}
